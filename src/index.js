3b68e1151c21410b9020a036672ddd9bafb54d67
MaybeJod <135610495+MaybeJod@users.noreply.github.com>
2024-12-09 15:32:41 +0100

    fix: product buttons are now connected to cart

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import "./assets/styles/hero.css";
 import "./assets/styles/checkoutPage.css";
 import "./assets/styles/topRated.css";
 import "./assets/styles/products.css";
-import "./assets/styles/productDetails.css"
-import "./assets/styles/filterFunction.css"
+import "./assets/styles/productDetails.css";
+import "./assets/styles/filterFunction.css";
 
 /* product section */
 import "./assets/styles/productSection.css";
@@ -20,7 +20,7 @@ import "./assets/icons-and-logo/Material Icon Person.svg";
 /* fetch api */
 import fetchProduct from "./assets/js/fetchProduct";
 /* display checkoutpage */
-import checkoutPage from "./assets/js/checkoutPage";
+//import checkoutPage from "./assets/js/checkoutPage";
 /* checkoutPage(); */
 /* reset content */
 import resetContent from "./assets/js/components/resetContent.js";
@@ -32,18 +32,20 @@ import { fetchAndDisplayProducts } from "./assets/js/fetchProduct";
 
 /* landing page products */
 import { displayCategoriesOnLandingPage } from "./assets/js/productDisplay_LP.js";
-import {registerLowToHigh, registerHighToLow, sortOnTheBasisOfRating }from "./assets/js/filterFunction.js"
+import {
+	registerLowToHigh,
+	registerHighToLow,
+	sortOnTheBasisOfRating,
+} from "./assets/js/filterFunction.js";
 
 /* landing page */
 import { createLandingPage } from "./assets/js/landingPage.js";
 import { createTrendingSection } from "./assets/js/trendingProducts.js";
 
+import { createTopRatedGrid } from "./assets/js/landing-page/topRated.js";
 
-
-import { createTopRatedGrid } from './assets/js/landing-page/topRated.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    createTopRatedGrid('https://fakestoreapi.com/products');
+document.addEventListener("DOMContentLoaded", () => {
+	createTopRatedGrid("https://fakestoreapi.com/products");
 });
 
 /* display product */
@@ -52,7 +54,6 @@ document.addEventListener('DOMContentLoaded', () => {
 import { sortProducts } from "./assets/js/productCategories";
 
 /* import {removeDivFromLandingPage} from "./assets/js/productCategories"; */
-
 
 /* cart */
 import renderCartPage from "./assets/js/cart/cartPage.js";
@@ -91,125 +92,122 @@ document.addEventListener("DOMContentLoaded", () => {
 	createLandingPage();
 	createTrendingSection();
 	displayCategoriesOnLandingPage();
-	registerButton();
+	productButtons();
 });
 
-
 /* nav drop down for categories */
-const buttonAllProducts = document.querySelector("#product-all");
-buttonAllProducts.addEventListener("click", () => {
+//const buttonAllProducts = document.querySelector("#product-all");
+/* buttonAllProducts.addEventListener("click", () => {
 	const productSectionElement = document.querySelector("#productSection");
 	resetContent();
 	fetchAndDisplayProducts(productSectionElement);
-});
+}); */
 
 //[document.querySelector('.a-class'), document.querySelector('.another-class')].forEach(item => {
- // item.addEventListener('click', event => {
-    //handle click
- // })
+// item.addEventListener('click', event => {
+//handle click
+// })
 //})
 //const buttonMen = document.querySelector("#product-men");
 
-
-const buttonWomen = document.querySelector("#product-women");
-buttonWomen.addEventListener("click", () => {
-	resetContent();
-	const apiUrl_women =
-		"https://fakestoreapi.com/products/category/women's clothing";
-		fetchProduct(apiUrl_women).then((products) => {
-		sortProducts(products);
-		const lowToHighPrice = document.querySelector('#lowToHigh');
-		registerLowToHigh(lowToHighPrice, products)
-		const highToLowPrice = document.querySelector('#highToLow');
-		registerHighToLow(highToLowPrice, products);
-		const filterByRating = document.querySelector('#filter_ratingBtn');
-		sortOnTheBasisOfRating(filterByRating,  products);
-	});
-});
-
-function registerButton(){
-	[document.querySelector('#shop_all'), document.getElementById('allProducts_btn')].forEach(item =>{
+function productButtons() {
+	[
+		document.querySelector("#product-all"),
+		document.querySelector("#shop_all"),
+		document.getElementById("allProducts_btn"),
+	].forEach((item) => {
 		item.addEventListener("click", () => {
-	resetContent();
-	const apiUrl_allProducts = 'https://fakestoreapi.com/products';
-	fetchProduct(apiUrl_allProducts).then((products) => {
-	sortProducts(products);
-	const lowToHighPrice = document.querySelector('#lowToHigh');
-	registerLowToHigh(lowToHighPrice, products);
-	const highToLowPrice = document.querySelector('#highToLow');
-	registerHighToLow(highToLowPrice, products);
-	const filterByRating = document.querySelector('#filter_ratingBtn');
-	sortOnTheBasisOfRating(filterByRating,  products)
+			resetContent();
+			const apiUrl_allProducts = "https://fakestoreapi.com/products";
+			fetchProduct(apiUrl_allProducts).then((products) => {
+				sortProducts(products);
+				const lowToHighPrice = document.querySelector("#lowToHigh");
+				registerLowToHigh(lowToHighPrice, products);
+				const highToLowPrice = document.querySelector("#highToLow");
+				registerHighToLow(highToLowPrice, products);
+				const filterByRating = document.querySelector("#filter_ratingBtn");
+				sortOnTheBasisOfRating(filterByRating, products);
+			});
+		});
 	});
-});
-});
-[document.querySelector("#product-men"), document.querySelector('#men_category')].forEach(item =>{
-	item.addEventListener("click", () => {
-		resetContent();
-		const apiUrl_men = 
-		"https://fakestoreapi.com/products/category/men's clothing";
-		fetchProduct(apiUrl_men).then((products) => {
-		sortProducts(products);
-		const lowToHighPrice = document.querySelector('#lowToHigh');
-		registerLowToHigh(lowToHighPrice, products);
-		const highToLowPrice = document.querySelector('#highToLow');
-		registerHighToLow(highToLowPrice, products);
-		const filterByRating = document.querySelector('#filter_ratingBtn');
-		sortOnTheBasisOfRating(filterByRating,  products)
+	[
+		document.querySelector("#product-men"),
+		document.querySelector("#men_category"),
+	].forEach((item) => {
+		item.addEventListener("click", () => {
+			resetContent();
+			const apiUrl_men =
+				"https://fakestoreapi.com/products/category/men's clothing";
+			fetchProduct(apiUrl_men).then((products) => {
+				sortProducts(products);
+				const lowToHighPrice = document.querySelector("#lowToHigh");
+				registerLowToHigh(lowToHighPrice, products);
+				const highToLowPrice = document.querySelector("#highToLow");
+				registerHighToLow(highToLowPrice, products);
+				const filterByRating = document.querySelector("#filter_ratingBtn");
+				sortOnTheBasisOfRating(filterByRating, products);
+			});
+		});
 	});
-});
-});
-//--------
-[document.querySelector("#product-women"), document.querySelector('#women_category')].forEach(item =>{
+	//--------
+	[
+		document.querySelector("#product-women"),
+		document.querySelector("#women_category"),
+	].forEach((item) => {
+		item.addEventListener("click", () => {
+			resetContent();
+			const apiUrl_women =
+				"https://fakestoreapi.com/products/category/women's clothing";
+			fetchProduct(apiUrl_women).then((products) => {
+				sortProducts(products);
+				const lowToHighPrice = document.querySelector("#lowToHigh");
+				registerLowToHigh(lowToHighPrice, products);
+				const highToLowPrice = document.querySelector("#highToLow");
+				registerHighToLow(highToLowPrice, products);
+				const filterByRating = document.querySelector("#filter_ratingBtn");
+				sortOnTheBasisOfRating(filterByRating, products);
+			});
+		});
+	});
 
-	item.addEventListener("click", () => {
-	resetContent();
-	const apiUrl_women =
-	"https://fakestoreapi.com/products/category/women's clothing";
-	fetchProduct(apiUrl_women).then((products) => {
-	sortProducts(products);
-	const lowToHighPrice = document.querySelector('#lowToHigh');
-		registerLowToHigh(lowToHighPrice, products);
-		const highToLowPrice = document.querySelector('#highToLow');
-		registerHighToLow(highToLowPrice, products);
-		const filterByRating = document.querySelector('#filter_ratingBtn');
-		sortOnTheBasisOfRating(filterByRating,  products)
+	//------------
+	[
+		document.querySelector("#product-accessories"),
+		document.querySelector("#accessories_category"),
+	].forEach((item) => {
+		item.addEventListener("click", () => {
+			resetContent();
+			const apiUrl_jewelery =
+				"https://fakestoreapi.com/products/category/jewelery";
+			fetchProduct(apiUrl_jewelery).then((products) => {
+				sortProducts(products);
+				const lowToHighPrice = document.querySelector("#lowToHigh");
+				registerLowToHigh(lowToHighPrice, products);
+				const highToLowPrice = document.querySelector("#highToLow");
+				registerHighToLow(highToLowPrice, products);
+				const filterByRating = document.querySelector("#filter_ratingBtn");
+				sortOnTheBasisOfRating(filterByRating, products);
+			});
+		});
 	});
-});
-});
-
-//------------
-[document.querySelector("#product-accessories"), document.querySelector('#accessories_category')].forEach(item =>{
-	item.addEventListener("click", () => {
-		resetContent();
-		const apiUrl_jewelery = "https://fakestoreapi.com/products/category/jewelery";
-		fetchProduct(apiUrl_jewelery).then((products) => {
-		sortProducts(products);
-		const lowToHighPrice = document.querySelector('#lowToHigh');
-		registerLowToHigh(lowToHighPrice, products);
-		const highToLowPrice = document.querySelector('#highToLow');
-		registerHighToLow(highToLowPrice, products);
-		const filterByRating = document.querySelector('#filter_ratingBtn');
-		sortOnTheBasisOfRating(filterByRating,  products)
-		});	
-});
-});
-//----------
-[document.querySelector("#product-electronics"), document.querySelector('#electronics_category')].forEach(item =>{
-	item.addEventListener("click", () => {
-	resetContent();
-	const apiUrl_electronics ="https://fakestoreapi.com/products/category/electronics";
-	fetchProduct(apiUrl_electronics).then((products) => {
-		sortProducts(products);
-		const lowToHighPrice = document.querySelector('#lowToHigh');
-		registerLowToHigh(lowToHighPrice, products);
-		const highToLowPrice = document.querySelector('#highToLow');
-		registerHighToLow(highToLowPrice, products);
-		const filterByRating = document.querySelector('#filter_ratingBtn');
-		sortOnTheBasisOfRating(filterByRating,  products)
+	//----------
+	[
+		document.querySelector("#product-electronics"),
+		document.querySelector("#electronics_category"),
+	].forEach((item) => {
+		item.addEventListener("click", () => {
+			resetContent();
+			const apiUrl_electronics =
+				"https://fakestoreapi.com/products/category/electronics";
+			fetchProduct(apiUrl_electronics).then((products) => {
+				sortProducts(products);
+				const lowToHighPrice = document.querySelector("#lowToHigh");
+				registerLowToHigh(lowToHighPrice, products);
+				const highToLowPrice = document.querySelector("#highToLow");
+				registerHighToLow(highToLowPrice, products);
+				const filterByRating = document.querySelector("#filter_ratingBtn");
+				sortOnTheBasisOfRating(filterByRating, products);
+			});
+		});
 	});
-});
-});
 }
-
-
